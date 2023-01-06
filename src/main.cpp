f96aab34f09b55f041c8b4c04169cf2b608b8230
Anna Dunster <anna@annardunster.com>
2023-01-05 18:04:17 -0800

    Add command line arg config for doctest

--- src/main.cpp
+++ src/main.cpp
@@ -3,18 +3,29 @@
 #define DOCTEST_CONFIG_IMPLEMENT
 #include "include/doctest.h"
 
-int main()
+int main(int argc, char **argv)
 {
     doctest::Context context;
+    // TODO: Figure out how to default this to 'don't run tests' in production https://github.com/doctest/doctest/blob/master/doc/markdown/main.md
+    context.applyCommandLine(argc, argv);
 
-    int res = context.run();
+    int doctestResult = context.run();
+
+    if (context.shouldExit())
+    {
+        return doctestResult;
+    }
+
     std::cout << "Hola" << std::endl;
 
-    return 0;
+    int client_stuff_return_code = 0;
+    // your program - if the testing framework is integrated in your production code
+
+    return doctestResult + client_stuff_return_code;
 };
 
 TEST_CASE("Verifying tests run")
 {
     CHECK(1 == 1);
-    CHECK(1 == 2);
+    // CHECK(1 == 2);
 }
