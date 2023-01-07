829f59b7d10c62ca14df2492756833ec12193456
Anna Dunster <anna@annardunster.com>
2023-01-07 14:01:47 -0800

    Add failing test for parse ENCOUNTER

--- dmc/include/dmc/UserInterface.hpp
+++ dmc/include/dmc/UserInterface.hpp
@@ -1,7 +1,16 @@
 #pragma once
+#include <iostream>
 
 class UserInterface
 {
 public:
+    enum class Command
+    {
+        INVALID = -1,
+        ENCOUNTER = 1
+    };
+
     int Interface();
+
+    Command parse(std::string input);
 };
--- dmc/src/UserInterface.cpp
+++ dmc/src/UserInterface.cpp
@@ -1,8 +1,35 @@
 #include <iostream>
+#include "../include/dependencies/doctest.h"
 #include "../include/dmc/UserInterface.hpp"
 
 int UserInterface::Interface()
 {
     std::cout << "Running Interface function" << std::endl;
     return 0;
+};
+
+UserInterface::Command UserInterface::parse(std::string input)
+{
+    return UserInterface::Command::INVALID;
+};
+
+TEST_CASE("Interface returns 0")
+{
+    UserInterface userInterface;
+    CHECK(userInterface.Interface() == 0);
+};
+
+TEST_SUITE("UserInterface Parse")
+{
+    UserInterface userInterface;
+
+    TEST_CASE("Defaults to returning INVALID")
+    {
+        CHECK(userInterface.parse("Steve") == UserInterface::Command::INVALID);
+    };
+
+    TEST_CASE("Returns ENCOUNTER when input is encounter")
+    {
+        CHECK(userInterface.parse("encounter") == UserInterface::Command::ENCOUNTER);
+    };
 };
